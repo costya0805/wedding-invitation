4e9c0fffd7e5e7b44fa8f94bb34252ea65d0b014
Konstantin <costya.co4ergin@gmail.com>
2025-03-24 23:46:17 +0500

    one more

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        попатка не пытка
       </p>
     </>
   )
