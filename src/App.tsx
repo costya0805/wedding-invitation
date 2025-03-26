fa89ed0fdf4d04203246e14f4a9a731eb0a71b0e
DashaD <ddigtyarenko@naumen.ru>
2025-03-26 23:52:24 +0500

    Envelope done!

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import {useState} from 'react'
-import './App.css'
-import envelope from './assets/images/envelope.png';
-import stamp from './assets/images/stamp.png';
+import './App.css';
+import {Envelope} from "./components/Envelope/Envelope.tsx";
 
 const App = () => {
-    const [isEnvelopeOpen, setIsEnvelopeOpen] = useState<boolean>(false)
 
     return (
         <div className={'main'}>
-            <div onClick={() => setIsEnvelopeOpen(true)} className={'envelope'}>
-                <img src={envelope} className={'envelopeImg'} alt={'envelope'}/>
-                <img src={stamp} className={`stamp ${isEnvelopeOpen ? 'stmapHidden' : ''}`} alt={'stamp'}/>
-            </div>
+            <Envelope/>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
