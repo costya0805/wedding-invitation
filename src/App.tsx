aa9f7317fff76564084f993dff44556bbe37d89a
DashaD <ddigtyarenko@naumen.ru>
2025-03-26 10:03:20 +0500

    Add envelope

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
+import envelope from './assets/images/envelope.png';
+import stamp from './assets/images/stamp.png';
 
 const App = () => {
-  const [count, setCount] = useState<number>(0)
+    const [isEnvelopeOpen, setIsEnvelopeOpen] = useState<boolean>(false)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        попатка не попытка
-      </p>
-    </>
-  )
+    return (
+        <div className={'main'}>
+            <div onClick={() => setIsEnvelopeOpen(true)} className={'envelope'}>
+                <img src={envelope} className={'envelopeImg'} alt={'envelope'}/>
+                <img src={stamp} className={`stamp ${isEnvelopeOpen ? 'stmapHidden' : ''}`} alt={'stamp'}/>
+            </div>
+        </div>
+    )
 }
 
 export default App
